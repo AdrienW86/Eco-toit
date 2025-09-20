1bfc9963b48b4cf54568a4de900f4cbd6b909c09
AdrienW86 <adrien_weiss@outlook.fr>
2025-09-20 14:40:22 +0200

    mise en ligne

---
+++ components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import styles from './card.module.css'
+
+
+export default function Card(props) {
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.txt}>{props.title}</h3>
+      <Image
+        src={props.src}
+        alt={props.alt}
+        width={300}
+        height={300}
+        sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
+      /> 
+    </div>
+  )
+}
--- components/Equipment/Equipment.js
+++ components/Equipment/Equipment.js
@@ -1,74 +1,53 @@
 import React from 'react'
-import Image from 'next/image'
-import styles from "./equipment.module.css"
+import Card from '../Card/Card'
+import styles from './equipment.module.css'
+
+const data = [
+  {
+    title: "Travaux de couverture",
+    src: "/couverture.jpg",
+    alt: "Illustration couverture de toit"
+  },
+  {
+    title: "Nettoyage écologique de toiture et façades",
+     src: "/facade.jpg",
+    alt: "Illustration balai vapeur"
+  },
+  {
+    title: "Peinture de toiture isolante",
+    src: "/facade.jpg",
+    alt: "Illustration lingettes microfibres",
+    link: "/peinture-isolante"
+  },
+   {
+    title: "Rénovation de façades",
+    src: "/facade.jpg",
+    alt: "Illustration rénovation de façade"
+  },
+  {
+    title: "Travaux de zinguerie",
+    src: "/zinguerie.jpg",
+    alt: "Illustration balai vapeur"
+  },
+  {
+    title: "Petite maçonnerie",
+     src: "/facade.jpg",
+    alt: "Illustration lingettes microfibres"
+  }
+]
 
 export default function Equipment() {
   return (
     <section className={styles.container}>
-      <div className={styles.column}>
-        <div className={styles.box}>
-                <Image
-                    src="/parking.png"
-                    alt="logo parking"
-                    width={20}
-                    height={20}
-                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                />
-                <p className={styles.txt}> Travaux de couverture </p>
-            </div>
-            <div className={styles.box}>
-                <Image
-                    src="/nettoyage.png"
-                    alt="illustration nettoyage"
-                    width={20}
-                    height={20}
-                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                />
-                <p className={styles.txt}> Nettoyage de toiture et façades </p>
-            </div>
-            <div className={styles.box}>
-                <Image
-                    src="/pool.png"
-                    alt="logo pisicne"
-                    width={20}
-                    height={20}
-                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                />
-                <p className={styles.txt}> Peinture de toiture isolante </p>
-            </div>
-        </div>
-        <div className={styles.column}>
-        <div className={styles.box}>
-                <Image
-                    src="/bar2.png"
-                    alt="logo bar"
-                    width={20}
-                    height={20}
-                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                />
-                <p className={styles.txt}> Rénovation de façades </p>
-            </div>
-            <div className={styles.box}>
-                <Image
-                    src="/zinguerie.png"
-                    alt="illustration d'une gouttière"
-                    width={20}
-                    height={20}
-                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                />
-                <p className={styles.txt}> Travaux de zinguerie </p>
-            </div>
-            <div className={styles.box}>
-                <Image
-                    src="/maconnerie.png"
-                    alt="illustration maçonnerie"
-                    width={20}
-                    height={20}
-                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                />
-                <p className={styles.txt}> Petite maçonnerie </p>
-            </div>
-        </div>
+      {data.map((item, index) => (
+        <Card 
+          key={index}
+          title={item.title}
+          description={item.description}
+          src={item.src}
+          alt={item.alt}
+        />
+      ))}
     </section>
   )
 }
